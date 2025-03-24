5a07f776510e7cc101a811a77692bba964015a18
NivasMalligesan <nivasmalligesan23@gmail.com>
2025-03-24 19:37:45 +0530

    COmpleted

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,5 +1,5 @@
 import validator from 'validator';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { v2 as cloudinary } from 'cloudinary';
 import parkingModel from '../models/parkingModel.js';
 import multer from 'multer';
