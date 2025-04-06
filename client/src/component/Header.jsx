2dc635b1b26c8df1e472f99bd500ccfa47807986
NivasMalligesan <nivasmalligesan23@gmail.com>
2025-04-06 18:29:06 +0530

    wanna change some things

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -11,16 +11,19 @@ const Header = () => {
     whileInView={{ opacity: 1, scale: 1 }} // Animation when in view
     transition={{ duration: 0.5 }}
     viewport={{ once: true,}} // Smooth transition
-    className='sm:mt-20 flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20 relative'
+    className='sm:mt-20 mt-5 flex md:flex-row  bg-primary rounded-lg px-6 md:px-10 lg:px-20 relative'
     >
         {/* Left side of the header */}
-        <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-20 md:py-[5vw]'>
-            <p className=' text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight'>
+        <div className='w-1/2 flex flex-col items-start justify-center gap-4 py-10 sm:py-20 md:py-[5vw]'>
+            <p className=' text-xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight'>
                 Find, Reserve and Park<br /> Parking at Your Fingertips
             </p>
             <div className='flex items-center gap-5'>
-                <img src={assets.group_profiles} alt="" className="w-32 h-auto" />
-                <p className='text-white text-sm'>
+                <img src={assets.group_profiles} alt="" className="w-15  sm:w-32 h-auto" />
+                <p className='text-white sm:hidden text-xs'>
+                  100+ Plus Trusted CUstomers
+                </p>
+                <p className='text-white hidden sm:block text-sm'>
                     Book parking spots in seconds <br className='hidden sm:block'/> Say goodbye to parking hassles with real-time availability!
                 </p>
             </div>
@@ -29,8 +32,8 @@ const Header = () => {
             </a>
         </div>
         {/* right sidee */}
-        <div className='md:w-1/2 relative'>
-          <img className='w-full md:absolute bottom-0 h-auto rounded-lg' src={assets.header} alt="" />
+        <div className='w-1/2 relative'>
+          <img className='w-full absolute bottom-0 h-auto rounded-lg' src={assets.header} alt="" />
         </div>
 
     </motion.div>
