33cbd736a99c7f6b5616db264d617259afd99a1e
NivasMalligesan <nivasmalligesan23@gmail.com>
2025-04-09 09:27:08 +0530

    updates

--- client/src/component/MobileNavbar.jsx
+++ client/src/component/MobileNavbar.jsx
@@ -1,40 +1,65 @@
-import React from 'react'
-import { NavLink, useNavigate, useLocation } from 'react-router-dom';
-import { Car, Contact, Globe, HistoryIcon, Home, Instagram, Linkedin, List, ParkingCircle } from 'lucide-react';
-
+import React from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import { Car, Contact, Globe, HistoryIcon, Home, Instagram, Linkedin, List, ParkingCircle, User } from 'lucide-react';
 
 const MobileNavbar = () => {
-        const location = useLocation();
+  const location = useLocation();
+
   return (
-    <div>
-        <div className={` md:hidden bottom-0 w-full fixed  right-0 pb-2 z-20 overflow-hidden bg-white transition-all shadow`}>
-            
-            <ul className='flex justify-around mt-2 px-2  text-lg font-medium'>
-                <NavLink to={'/'}  className={`${location.pathname === '/' ? 'bg-primary text-white  cursor-pointer' : 'cursor-pointer w-full '} rounded flex flex-col items-center w-full gap-1  rounded p-3 text-nowrap `}>
-                    <Home className='text-primary' size={20} />
-                    <p className=" rounded text-xs  ">Home</p>
-                </NavLink>
-                <NavLink to={'/parkings'} className={`${location.pathname === '/parkings' ? ' bg-primary cursor-pointer text-white ' : 'cursor-pointer w-full  '} flex flex-col items-center w-full  gap-1 rounded p-3 text-nowrap`}>
-                    <ParkingCircle className='text-primary' size={20} />
-                    <p className=" rounded  text-xs">All Parkings</p>
-                </NavLink>
-                <NavLink to={'/my-bookings'} className={`${location.pathname === '/my-bookings' ? ' bg-primary cursor-pointer text-white ' : 'cursor-pointer w-full  '}rounded flex flex-col items-center w-full gap-1 p-3 text-nowrap`}>
-                    <HistoryIcon className='text-primary' size={20} />
-                    <p className="rounded text-xs">My Bookings</p>
-                </NavLink>
-                <NavLink to={'/contact'} className={`${location.pathname === '/contact' ? ' bg-primary cursor-pointer text-white w-full ' : 'cursor-pointer w-full  '}rounded flex flex-col items-center w-full gap-1 p-3 text-nowrap`}>
-                    <Contact className='text-primary' size={20} />
-                    <p className=" rounded text-xs">Contact</p>
-                </NavLink>
-                <NavLink to={'/my-profile'} className={`${location.pathname === '/my-profile' ? ' bg-primary cursor-pointer text-white w-full ' : 'cursor-pointer w-full  '}rounded flex flex-col items-center w-full gap-1 p-3 text-nowrap`}>
-                    <Contact className='text-primary' size={20} />
-                    <p className=" rounded text-xs">Profile</p>
-                </NavLink>
-            </ul>
+    <div className="md:hidden fixed bottom-0 w-full right-0 pb-2 z-20 bg-white shadow transition-all">
+      <ul className="flex justify-around mt-2 px-2 text-lg font-medium">
+        <NavLink
+          to="/"
+          className={`${
+            location.pathname === '/' ? 'bg-primary text-white' : ''
+          } flex flex-col items-center w-full gap-1 rounded p-3 cursor-pointer`}
+        >
+          <Home className="text-primary" size={20} />
+          <p className="text-xs">Home</p>
+        </NavLink>
 
-                </div>
+        <NavLink
+          to="/parkings"
+          className={`${
+            location.pathname === '/parkings' ? 'bg-primary text-white' : ''
+          } flex flex-col items-center w-full gap-1 rounded p-3 cursor-pointer`}
+        >
+          <ParkingCircle className="text-primary" size={20} />
+          <p className="text-xs">All Parkings</p>
+        </NavLink>
+
+        <NavLink
+          to="/my-bookings"
+          className={`${
+            location.pathname === '/my-bookings' ? 'bg-primary text-white' : ''
+          } flex flex-col items-center w-full gap-1 rounded p-3 cursor-pointer`}
+        >
+          <HistoryIcon className="text-primary" size={20} />
+          <p className="text-xs">My Bookings</p>
+        </NavLink>
+
+        <NavLink
+          to="/contact"
+          className={`${
+            location.pathname === '/contact' ? 'bg-primary text-white' : ''
+          } flex flex-col items-center w-full gap-1 rounded p-3 cursor-pointer`}
+        >
+          <Contact className="text-primary" size={20} />
+          <p className="text-xs">Contact</p>
+        </NavLink>
+
+        <NavLink
+          to="/my-profile"
+          className={`${
+            location.pathname === '/my-profile' ? 'bg-primary text-white' : ''
+          } flex flex-col items-center w-full gap-1 rounded p-3 cursor-pointer`}
+        >
+          <User className="text-primary" size={20} />
+          <p className="text-xs">Profile</p>
+        </NavLink>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default MobileNavbar
+export default MobileNavbar;
